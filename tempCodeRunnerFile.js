852db76231be728d9f8bf25a4f50455b8a2b1056
topurayhan007 <topurayhan007@gmail.com>
2023-02-06 23:34:38 +0600

    All problems done

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-console.log(gemsToDiamond(34, 45, 10));
-// console.log(gemsToDiamond(1, 1, 1));
-// console.log(gemsToDiamond(20, 200, 50));
-// console.log(gemsToDiamond(34, 45));
-// console.log(gemsToDiamond("hello"));
