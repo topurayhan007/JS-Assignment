9b580df5046cdba460fbad25e59925441751879b
topurayhan007 <topurayhan007@gmail.com>
2023-02-06 23:34:25 +0600

    All problems done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
-// Problem 1:
+// Problem 1: The function mindGame() takes a number as parameter and series of calculation is performed.
+// The input is first multiplied by 3, then the total is added with 10, then the total is divided by 2,
+// then the total is subtracted by 5 and then it is returned.
 
 function mindGame(number) {
   // Checks if the input is a number and is positive else returns a error message
@@ -25,11 +27,14 @@ function mindGame(number) {
 // mindGame function tested
 // console.log(mindGame("h"));
 // console.log(mindGame(-10));
-// console.log(mindGame(10));
+// console.log(mindGame(5));
+// console.log(mindGame(50));
+// console.log(mindGame(33));
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Problem 2:
+// Problem 2: The function evenOdd() takes a string as parameter and checks whether the string length
+// is odd or even and returns accordingly.
 
 function evenOdd(input) {
   // checks if the input is a string else returns a error message
@@ -38,9 +43,9 @@ function evenOdd(input) {
     let inputLength = input.length;
     // checks if the length value is odd or even and returns accordingly
     if (inputLength % 2 === 0) {
-      return "Even";
+      return "even";
     } else {
-      return "Odd";
+      return "odd";
     }
   } else {
     return "Please enter a string!";
@@ -54,7 +59,10 @@ function evenOdd(input) {
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Problem 3:
+// Problem 3: The function isLGSeven() takes a number as parameter which is deducted by 7,
+// and it checks whether the absolute value of the deduction is greater or equal to 7.
+// If it is then the double of the absolute value is returned else
+// the value that is left after deduction is returned.
 
 function isLGSeven(number) {
   // checks if the input is a number else returns a error message
@@ -84,7 +92,9 @@ function isLGSeven(number) {
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Problem 4:
+// Problem 4: The function findingBadData() takes an array and checks whether there is any bad data in it
+// and the total number of bad data is returned.
+// Bad data here means if the element of the array is less than 0
 
 function findingBadData(arr) {
   // checks whether the input is an array else returns a error message
@@ -114,18 +124,44 @@ function findingBadData(arr) {
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Problem 5:
+// Problem 5: The function gemsToDiamond() take 3 parameters and the parameters are multiplied by
+// a specific value 21, 32, 43 respectively to covert the gem numbers to diamonds.
+// All diamonds are added and the total is checked whether it's greater or equal to 2000, if it's then
+// 2000 is deducted from the total and the result is returned else the total is returned.
 
-function gemsToDiamond(friend1, friend2, friend3) {
+function gemsToDiamond(friend1Gem, friend2Gem, friend3Gem) {
+  // checks whether all inputs are numbers else return a error message
   if (
-    typeof friend1 === "number" &&
-    typeof friend2 === "number" &&
-    typeof friend3 === "number"
+    typeof friend1Gem === "number" &&
+    typeof friend2Gem === "number" &&
+    typeof friend3Gem === "number"
   ) {
-    return "hello";
+    // all friends' diamonds are calculated accordingly
+    let friend1Diamond = friend1Gem * 21;
+    let friend2Diamond = friend2Gem * 32;
+    let friend3Diamond = friend3Gem * 43;
+
+    // total diamond is calculated
+    const totalDiamond = friend1Diamond + friend2Diamond + friend3Diamond;
+
+    // checks whether total diamond is greater or equal to 2000
+    // if it is then 2000 is subtracted from the total and result is returned
+    // else total diamond is returned
+    if (totalDiamond >= 2000) {
+      const finalDiamond = totalDiamond - 2000;
+
+      return finalDiamond;
+    } else {
+      return totalDiamond;
+    }
   } else {
     return "Please enter 3 numbers!";
   }
 }
 
 // gemsToDiamond function tested
+// console.log(gemsToDiamond(34, 45, 10));
+// console.log(gemsToDiamond(1, 1, 1));
+// console.log(gemsToDiamond(20, 200, 50));
+// console.log(gemsToDiamond(34, 45));
+// console.log(gemsToDiamond("hello"));
